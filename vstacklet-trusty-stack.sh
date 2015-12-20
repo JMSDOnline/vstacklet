8a949acf32f2d3e2f71121e77b9831b167254bfa
JMSDOnline <jmsolodesigns@gmail.com>
2015-12-19 18:03:57 -0600

    output logging breaks archive downloads

--- vstacklet-trusty-stack.sh
+++ vstacklet-trusty-stack.sh
@@ -122,8 +122,8 @@ echo
 # package and repo addition (b) _add signed keys_
 echo "${sub_title}Installing signed keys ... ${normal}"
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db >>"${OUTTO}" 2>&1;
-curl https://repo.varnish-cache.org/ubuntu/GPG-key.txt | apt-key add -
-curl http://nginx.org/keys/nginx_signing.key | apt-key add -
+curl https://repo.varnish-cache.org/ubuntu/GPG-key.txt | apt-key add - >/dev/null 2>&1
+curl http://nginx.org/keys/nginx_signing.key | apt-key add - >/dev/null 2>&1
 echo "${OK}"
 echo
 
@@ -165,7 +165,7 @@ echo "${sub_title}Installing and Configuring Nginx ... ${normal}"
 apt-get -y install nginx >>"${OUTTO}" 2>&1;
 service nginx stop >>"${OUTTO}" 2>&1;
 mv /etc/nginx /etc/nginx-previous >>"${OUTTO}" 2>&1;
-curl -L https://github.com/JMSDOnline/vstacklet-server-configs/archive/v0.1-alpha.tar.gz >>"${OUTTO}" 2>&1 | tar -xz >>"${OUTTO}" 2>&1;
+curl -L https://github.com/JMSDOnline/vstacklet-server-configs/archive/v0.1-alpha.tar.gz >/dev/null 2>&1 | tar -xz >>"${OUTTO}" 2>&1;
 mv vstacklet-server-configs-0.1-alpha /etc/nginx >>"${OUTTO}" 2>&1;
 cp /etc/nginx-previous/uwsgi_params /etc/nginx-previous/fastcgi_params /etc/nginx >>"${OUTTO}" 2>&1;
 # sed -i.bak -e
@@ -182,7 +182,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   conf1="  listen [::]:443 ssl http2;\n  listen *:443 ssl http2;\n"
   conf2="  include vstacklet/directive-only/ssl.conf;\n  ssl_certificate /etc/ssl/certs/$sitename.crt;\n  ssl_certificate_key /etc/ssl/private/$sitename.key;"
   openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/nginx/private/$sitename.key -out /etc/ssl/nginx/certs/$sitename.crt
-  chmod 400 /etc/ssl/nginx/private/$sitename.key >>"${OUTTO}" 2>&1;
+  chmod 400 /etc/ssl/nginx/private/$sitename.key
 else
   conf1=
   conf2=
