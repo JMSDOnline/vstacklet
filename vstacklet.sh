2acbe7e0ef73d9d5de982628896000523be2b0d6
JMSDOnline <jmsolodesigns@gmail.com>
2016-02-12 18:27:03 -0600

    host fix

--- vstacklet.sh
+++ vstacklet.sh
@@ -12,18 +12,18 @@ mkdir -p vstacklet /backup/{directories,databases}
 cd vstacklet
 
 # Download the needed scripts for VStacklet
-curl -LO https://bitbucket.org/JMSDOnline/vstacklet/raw/vstacklet-trusty-stack.sh >/dev/null 2>&1;
-curl -LO https://bitbucket.org/JMSDOnline/vstacklet/raw/files-backup.sh >/dev/null 2>&1;
-curl -LO https://bitbucket.org/JMSDOnline/vstacklet/raw/database-backup.sh >/dev/null 2>&1;
-curl -LO https://bitbucket.org/JMSDOnline/vstacklet/raw/package-backups.sh >/dev/null 2>&1;
-curl -LO https://bitbucket.org/JMSDOnline/vstacklet/raw/backup-cleanup.sh >/dev/null 2>&1;
+curl -LO https://raw.githubusercontent.com/JMSDOnline/vstacklet/master/vstacklet-trusty-stack.sh >/dev/null 2>&1;
+curl -LO https://raw.githubusercontent.com/JMSDOnline/vstacklet/master/files-backup.sh >/dev/null 2>&1;
+curl -LO https://raw.githubusercontent.com/JMSDOnline/vstacklet/master/database-backup.sh >/dev/null 2>&1;
+curl -LO https://raw.githubusercontent.com/JMSDOnline/vstacklet/master/package-backups.sh >/dev/null 2>&1;
+curl -LO https://raw.githubusercontent.com/JMSDOnline/vstacklet/master/backup-cleanup.sh >/dev/null 2>&1;
 
 # Convert all shell scripts to executable
 chmod +x *.sh
 cd
 
 # Download VStacklet System Backup Executable
-curl -LO https://bitbucket.org/JMSDOnline/vstacklet/raw/vs-backup >/dev/null 2>&1;
+curl -LO https://raw.githubusercontent.com/JMSDOnline/vstacklet/master/vs-backup >/dev/null 2>&1;
 chmod +x vs-backup
 mv vs-backup /usr/local/bin
 
