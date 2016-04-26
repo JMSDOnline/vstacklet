1161dfdd2842511bb2d9697744722bd15ff4b10d
JMSDOnline <jmsolodesigns@gmail.com>
2016-04-26 14:28:13 -0500

    sort tabs

--- nginx-pagespeed.sh
+++ nginx-pagespeed.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+TAB=$'\t'
+
 if [[ "$USER" != 'root' ]]; then
 	echo "Sorry, you need to run this as root"
 	exit
 fi
 RELEASE_CDN=`lsb_release -sr`
 apt-get update
-apt-get -y --force-yes  install dpkg-dev build-essential zlib1g-dev libpcre3 libpcre3-dev unzip
+apt-get -y install dpkg-dev build-essential zlib1g-dev libpcre3 libpcre3-dev unzip
 if [[ "$RELEASE_CDN" = '14.04' ]]; then
 echo "deb http://nginx.org/packages/ubuntu/ trusty nginx" >> /etc/apt/sources.list.d/nginx.list
 echo "deb-src http://nginx.org/packages/ubuntu/ trusty nginx" >> /etc/apt/sources.list.d/nginx.list
@@ -13,7 +15,7 @@ echo "deb-src http://nginx.org/packages/ubuntu/ trusty nginx" >> /etc/apt/source
 fi
 if [[ "$RELEASE_CDN" =~ ("15.04"|"15.10"|"16.04") ]]; then
 echo "deb http://nginx.org/packages/mainline/ubuntu/ wily nginx" >> /etc/apt/sources.list.d/nginx.list
-echo "deb http://nginx.org/packages/mainline/ubuntu/ wily nginx" >> /etc/apt/sources.list.d/nginx.list
+echo "deb-src http://nginx.org/packages/mainline/ubuntu/ wily nginx" >> /etc/apt/sources.list.d/nginx.list
         exit
 fi
 
@@ -23,8 +25,8 @@ apt-get update
 
 mkdir -p ~/new/nginx_source/
 cd ~/new/nginx_source/
-apt-get -y --force-yes source nginx
-apt-get -y --force-yes build-dep nginx
+apt-get -y source nginx
+apt-get -y build-dep nginx
 
 mkdir -p ~/new/ngx_pagespeed/
 cd ~/new/ngx_pagespeed/
@@ -40,16 +42,16 @@ bash bush.sh
 tar -xzf *.tar.gz
 
 cd ~/new/nginx_source/nginx-*/debian/
-sed -i '22 a 	--add-module=../../ngx_pagespeed/ngx_pagespeed-master \\' rules
+sed -i '22 a ${TAB}--add-module=../../ngx_pagespeed/ngx_pagespeed-master \\' rules
 if [[ "$RELEASE_CDN" = '14.04' ]]; then
-sed -i '61 a 	--add-module=../../ngx_pagespeed/ngx_pagespeed-master \\' rules
+sed -i '61 a ${TAB}--add-module=../../ngx_pagespeed/ngx_pagespeed-master \\' rules
        exit
 fi
 
 if [[ "$RELEASE_CDN" =~ ("15.04"|"15.10"|"16.04") ]]; then
-sed -i '65 a 	--add-module=../../ngx_pagespeed/ngx_pagespeed-master \' rules
-sed -i '58 a 	--with-cc-opt=" -D_GLIBCXX_USE_CXX11_ABI=0" \' rules
-sed -i '101 a 	--with-cc-opt=" -D_GLIBCXX_USE_CXX11_ABI=0" \' rules
+sed -i '65 a ${TAB}--add-module=../../ngx_pagespeed/ngx_pagespeed-master \' rules
+sed -i '58 a ${TAB}--with-cc-opt=" -D_GLIBCXX_USE_CXX11_ABI=0" \' rules
+sed -i '101 a ${TAB}--with-cc-opt=" -D_GLIBCXX_USE_CXX11_ABI=0" \' rules
        exit
 fi
 cd ~/new/nginx_source/nginx-*/
@@ -57,11 +59,11 @@ dpkg-buildpackage -b
 cd ~/new/nginx_source/
 dpkg -i nginx_*amd64.deb
 nginx -V
-mkdir -p /var/ngx_pagespeed_cache
-chown -R www-data:www-data /var/ngx_pagespeed_cache
+mkdir -p /etc/nginx/ngx_pagespeed_cache
+chown -R www-data:www-data /etc/nginx/ngx_pagespeed_cache
 cd /etc/nginx/
 sed -i '30ipagespeed 	on;' nginx.conf
-sed -i '31ipagespeed 	FileCachePath /var/ngx_pagespeed_cache;' nginx.conf
+sed -i '31ipagespeed 	FileCachePath /etc/nginx/ngx_pagespeed_cache;' nginx.conf
 service nginx restart
 curl -I -p http://localhost|grep X-Page-Speed
 exit
