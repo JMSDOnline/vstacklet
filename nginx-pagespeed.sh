daf1929db2f5e70d6c6d74389c82f265b60ecd00
JMSDOnline <jmsolodesigns@gmail.com>
2016-04-26 23:11:28 -0500

    pagespeed cache directory path fix

--- nginx-pagespeed.sh
+++ nginx-pagespeed.sh
@@ -157,11 +157,11 @@ function _compnginx() {
 }
 
 function _setpsng() {
-	mkdir -p /var/ngx_pagespeed_cache
+	mkdir -p /etc/nginx/ngx_pagespeed_cache
 	chown -R www-data:www-data /etc/nginx/ngx_pagespeed_cache
 	cd /etc/nginx/
-	sed -i '30i \ \ \ \ \pagespeed on;' nginx.conf
-	sed -i '31i \ \ \ \ \pagespeed FileCachePath /etc/nginx/ngx_pagespeed_cache;' nginx.conf
+	sed -i '30i \ \ \ \ \#pagespeed on;' nginx.conf
+	sed -i '31i \ \ \ \ \#pagespeed FileCachePath /etc/nginx/ngx_pagespeed_cache;' nginx.conf
 }
 
 function _restartservice() {
