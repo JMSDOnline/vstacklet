02c7361b4aa0a4b8daeeae35e6c99cfd9b938c39
JMSolo <jmsolodesigns@gmail.com>
2016-08-19 07:56:08 -0500

    update csf download path as per issue comment #93

--- vstacklet-ubuntu-stack.sh
+++ vstacklet-ubuntu-stack.sh
@@ -738,7 +738,8 @@ function _csf() {
   if [[ ${csf} == "yes" ]]; then
     echo -n "${green}Installing and Adjusting CSF${normal} ... "
     apt-get -y install e2fsprogs >/dev/null 2>&1;
-    wget http://www.configserver.com/free/csf.tgz >/dev/null 2>&1;
+    wget https://download.configserver.com/csf.tgz
+    #wget http://www.configserver.com/free/csf.tgz >/dev/null 2>&1;
     tar -xzf csf.tgz >/dev/null 2>&1;
     ufw disable >>"${OUTTO}" 2>&1;
     cd csf
