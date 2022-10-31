0636d0b17e267d6c51861b4d07633ac972c708bb
JMSolo <jmsolodesigns@gmail.com>
2022-10-31 13:25:15 -0500

    dev: rewrite main vstacklet.sh to call in server-stack

---
+++ docs/setup/vstacklet.sh.md
@@ -0,0 +1,50 @@
+# vstacklet.sh - v3.1.1003
+
+
+---
+
+This script is designed to be run on a fresh Ubuntu 18.04/20.04 or
+Debian 9/10/11 server. I have done my best to keep it tidy and with as much
+error checking as possible. Couple this with loads of comments and you should
+have a pretty good idea of what is going on. If you have any questions,
+comments, or suggestions, please feel free to open an issue on GitHub.
+
+---
+
+vStacklet will install and configure the following:
+- NGinx 1.23.+ (HTTP Server)
+- PHP 7.4 (FPM) with common extensions
+- PHP 8.1 (FPM) with common extensions
+- MariaDB 10.6.+ (MySQL Database)
+- Varnish 7.2.+ (HTTP Cache)
+- CSF 14.+ (Config Server Firewall)
+- and more!
+
+---
+
+
+
+### setup::download()
+
+Setup the environment and download vStacklet
+
+notes:
+- This script function is responsible for downloading vStacklet from GitHub
+and setting up the environment for the installation.
+- This script function will also check for the existence of the required
+packages and install them if they are not found.
+- This script function will additionally call the server stack installation
+and process the given options/flags and arguments.
+- For the various setup options available: [vStacklet Server Stack Documentation](https://github.com/JMSDOnline/vstacklet/blob/development/docs/setup/vstacklet-server-stack.sh.md)
+
+#### options:
+
+-  $1 - the option/flag to process
+
+#### arguments:
+
+-  $2 - the value of the option/flag
+
+---
+
+
--- setup/vstacklet.sh
+++ setup/vstacklet.sh
@@ -1,93 +1,98 @@
 #!/bin/bash
+################################################################################
+# <START METADATA>
+# @file_name: vstacklet.sh
+# @version: 3.1.1003
+# @description: Lightweight script to quickly install a LEMP stack with Nginx,
+# Varnish, PHP7.4/8.1 (PHP-FPM), OPCode Cache, IonCube Loader, MariaDB, Sendmail
+# and more on a fresh Ubuntu 18.04/20.04 or Debian 9/10/11 server for
+# website-based server applications.
 #
-# [VStacklet Varnish LEMP Stack Prep Script]
+# @project_name: vstacklet
 #
-# GitHub:   https://github.com/JMSDOnline/vstacklet
-# Author:   Jason Matthews
-# URL:      https://jmsolodesigns.com/code-projects/vstacklet/varnish-lemp-stack
+# @brief: This script is designed to be run on a fresh Ubuntu 18.04/20.04 or
+# Debian 9/10/11 server. I have done my best to keep it tidy and with as much
+# error checking as possible. Couple this with loads of comments and you should
+# have a pretty good idea of what is going on. If you have any questions,
+# comments, or suggestions, please feel free to open an issue on GitHub.
 #
-# shellcheck disable=SC2068,1090,1091,2034,2312
-#################################################################################
-#server_ip=$(ip addr show | grep 'inet ' | grep -v 127.0.0.1 | awk '{print $2}' | cut -d/ -f1 | head -n 1);
-#hostname1=$(hostname -s);
-#################################################################################
-#Script Console Colors
-green=$(tput setaf 2)
-yellow=$(tput setaf 3)
-cyan=$(tput setaf 6)
-standout=$(tput smso)
-normal=$(tput sgr0)
-title=${standout}
-#################################################################################
-if [[ -f /usr/bin/lsb_release ]]; then
-	DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
-elif [[ -f "/etc/debian_version" ]]; then
-	DISTRO='Debian'
-fi
-#################################################################################
+# vStacklet will install and configure the following:
+# - NGinx 1.23.+ (HTTP Server)
+# - PHP 7.4 (FPM) with common extensions
+# - PHP 8.1 (FPM) with common extensions
+# - MariaDB 10.6.+ (MySQL Database)
+# - Varnish 7.2.+ (HTTP Cache)
+# - CSF 14.+ (Config Server Firewall)
+# - and more!
+#
+# @save_tasks:
+#  automated_versioning: true
+#  automated_documentation: true
+#
+# @build_tasks:
+#  automated_comment_strip: false
+#  automated_encryption: false
+#
+# @author: Jason Matthews (JMSolo)
+# @author_contact: https://github.com/JMSDOnline/vstacklet
+#
+# @license: MIT License (Included in LICENSE)
+# Copyright (C) 2016-2022, Jason Matthews
+# All rights reserved.
+# <END METADATA>
+################################################################################
+# shellcheck disable=SC2068,SC1091
+################################################################################
 
-# Set TERM to ~/.bashrc
-if ! grep -q "export TERM=xterm" /root/.bashrc; then
-	echo "export TERM=xterm" >>/root/.bashrc
-	source /root/.bashrc
-fi
+################################################################################
+# @name: setup::download
+# @description: Setup the environment and download vStacklet
+#
+# notes:
+# - This script function is responsible for downloading vStacklet from GitHub
+# and setting up the environment for the installation.
+# - This script function will also check for the existence of the required
+# packages and install them if they are not found.
+# - This script function will additionally call the server stack installation
+# and process the given options/flags and arguments.
+# - For the various setup options available: [vStacklet Server Stack Documentation](https://github.com/JMSDOnline/vstacklet/blob/development/docs/setup/vstacklet-server-stack.sh.md)
+#
+# @option: $1 - the option/flag to process
+# @arg: $2 - the value of the option/flag
+# @break
+################################################################################
 
-# Create vstacklet & backup directory strucutre
-mkdir -p /backup/{directories,databases}
-
-# Download VStacklet System Backup Executable
-chmod +x /etc/vstacklet/packages/backup/*
-mv vs-backup /usr/local/bin
-
-function _string() { perl -le 'print map {(a..z,A..Z,0..9)[rand 62] } 0..pop' 15; }
-
-function _askvstacklet() {
-	echo
-	echo
-	echo "${title} Welcome to the VStacklet LEMP stack install kit! ${normal}"
-	echo " version: ${VERSION}"
-	echo
-	echo "${bold} Enjoy the simplicity one script can provide to deliver ${normal}"
-	echo "${bold} you the essentials of a finely tuned server environment.${normal}"
-	echo "${bold} Nginx, Varnish, CSF, MariaDB w/ phpMyAdmin to name a few.${normal}"
-	echo "${bold} Actively maintained and quality controlled.${normal}"
-	echo
-	echo
-	echo -n "${bold}${yellow}Are you ready to install VStacklet for Ubuntu 18.04/20.04 & Debian 9/10/11 ?${normal} (${bold}${green}Y${normal}/n): "
-	read -r responce
-	case ${responce} in
-	[yY] | [yY][Ee][Ss] | "") vstacklet=yes ;;
-	[nN] | [nN][Oo]) vstacklet=no ;;
-	*)
-		echo "Please answer yes or no."
-		_askvstacklet
-		;;
-	esac
-}
-
-clear
-
-function _vstacklet() {
-	if [[ ${vstacklet} == "yes" ]]; then
-		DIR="/etc/vstacklet/setup/"
-		if [[ ! -d ${DIR} ]]; then DIR="${PWD}"; fi
-		. "${DIR}vstacklet-server-stack.sh"
+setup::download() {
+	whoami=$(whoami)
+	local -r vstacklet_url="https://github.com/JMSDOnline/vstacklet/raw/development/setup/vstacklet-server-stack.sh"
+	local -r vstacklet_path="/usr/local/bin/vstacklet-server-stack"
+	[[ ${whoami} != "root" ]] && { printf -- "%s\n" "ERROR: Install as root or via sudo." && exit 1; }
+	cd "${HOME}" || { printf -- "%s\n" "Error: Unable to move to ${HOME}" && exit 1; }
+	# Remove old install script
+	[[ -f ${vstacklet_path} ]] && rm -f "${vstacklet_path}"
+	# Adjust .bashrc TERM
+	if ! grep -q "export TERM=xterm" /root/.bashrc; then
+		echo -en "export TERM=xterm" >>"/root/.bashrc"
 	fi
+	source "${HOME}/.bashrc"
+	# Create vstacklet & backup directory strucutre
+	mkdir -p /backup/{directories,databases} /tmp/vstacklet_packages || { printf -- "%s\n" "Error: Unable to create /backup/{directories,databases} /tmp/vstacklet_packages" && exit 1; }
+	git clone https://github.com/JMSDOnline/vstacklet_packages.git /tmp/vstacklet_packages || { printf -- "%s\n" "Error: Unable to clone vstacklet_packages" && exit 1; }
+	# Send vStacklet backup (vs-backup) to /usr/local/bin
+	cp -f /tmp/vstacklet_packages/vs-backup /usr/local/bin/vs-backup || { printf -- "%s\n" "Error: Unable to copy vs-backup to /usr/local/bin" && exit 1; }
+	chmod +x /usr/local/bin/vs-backup || { printf -- "%s\n" "Error: Unable to chmod +x /usr/local/bin/vs-backup" && exit 1; }
+	# Run apt update and upgrade
+	DEBIAN_FRONTEND=noninteractive apt-get -yqq -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" update >/dev/null 2>&1
+	DEBIAN_FRONTEND=noninteractive apt-get -yqq -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade >/dev/null 2>&1 || { printf -- "%s\n" "Error: Unable to run apt-upgrade, please check your apt with 'apt -y update && apt -y upgrade'" && exit 1; }
+	# Install curl, sudo, and wget
+	apt-get -yqq install curl sudo wget --allow-unauthenticated >/dev/null 2>&1 || { printf -- "%s\n" "Error: Unable to install wget, sudo, and curl" && exit 1; }
+	# Download the installation script
+	curl -sSL "${vstacklet_url}" -o "${vstacklet_path}" || { printf -- "%s\n" "Error: Unable to download the installation script." && exit 1; }
+	# Make the installation script executable
+	chmod +x "${vstacklet_path}" || { printf -- "%s\n" "Error: Unable to make the installation script executable." && exit 1; }
+	# Execute the installation script
+	[[ -f "${vstacklet_path}" ]] && /"${vstacklet_path}" "$@" && rm -f "${HOME}/vstacklet.sh"
 }
 
-function _novstacklet() {
-	if [[ ${vstacklet} == "no" ]]; then
-		echo "${bold}${cyan}Cancelling install. If you would like to run this installer in the future${normal}"
-		echo "${bold}${cyan}type${normal} ${green}${bold}cd /etc/vstacklet/setup && ./vstacklet.sh${normal}"
-		echo "${bold}${cyan}followed by tapping Enter on your keyboard.${normal}"
-	fi
-}
-
-VERSION="3.1.0"
-
-_askvstacklet
-if [[ ${vstacklet} == "yes" ]]; then
-	echo -n "${bold}Installing VStacklet Kit for Ubuntu 18.04, 20.04 & Debian 9, 10, and 11 support${normal} ... " && _vstacklet
-elif [[ ${vstacklet} == "no" ]]; then
-	_novstacklet
-fi
+setup::download "$@"
+exit 1
