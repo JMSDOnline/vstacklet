2a7c0ddb0d14860934646d9dfe99d97c2039ff21
JMSolo <jmsolodesigns@gmail.com>
2023-05-02 08:56:36 -0500

    fix: correction to version and help calls
    changelog: fixes

--- docs/setup/vstacklet-server-stack.sh.md
+++ docs/setup/vstacklet-server-stack.sh.md
@@ -1,4 +1,4 @@
-# vstacklet-server-stack.sh - v3.1.2008
+# vstacklet-server-stack.sh - v3.1.2009
 
 
 ---
--- setup/vstacklet-server-stack.sh
+++ setup/vstacklet-server-stack.sh
@@ -2,7 +2,7 @@
 ##################################################################################
 # <START METADATA>
 # @file_name: vstacklet-server-stack.sh
-# @version: 3.1.2008
+# @version: 3.1.2009
 # @description: Lightweight script to quickly install a LEMP stack with Nginx,
 # Varnish, PHP7.4/8.1 (PHP-FPM), OPCode Cache, IonCube Loader, MariaDB, Sendmail
 # and more on a fresh Ubuntu 18.04/20.04 or Debian 9/10/11 server for
@@ -3538,8 +3538,8 @@ trap 'vstacklet::clean::rollback' SIGINT
 vstacklet::environment::init                                                    #(1)
 vstacklet::environment::functions                                               #(2)
 vstacklet::args::process "$@"                                                   #(3)
-[[ "$*" =~ "-h" ]] || [[ "$*" =~ "--help" ]] && vstacklet::help::display
-[[ "$*" =~ "-V" ]] || [[ "$*" =~ "--version" ]] && vstacklet::version::display
+#[[ "$*" =~ "-h" ]] || [[ "$*" =~ "--help" ]] && vstacklet::help::display
+#[[ "$*" =~ "-V" ]] || [[ "$*" =~ "--version" ]] && vstacklet::version::display
 vstacklet::log::check                                                           #(4)
 vstacklet::apt::update                                                          #(5)
 vstacklet::dependencies::install                                                #(6)
